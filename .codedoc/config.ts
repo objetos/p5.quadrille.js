de0985697d2eabc50546a7c15b6e005001d319b8
nakednous <nakednous@gmail.com>
2020-11-10 16:20:42 -0500

    config update

--- .codedoc/config.ts
+++ .codedoc/config.ts
@@ -1,16 +1,33 @@
-
 import { configuration } from '@codedoc/core';
-
 import { theme } from './theme';
 
-
-export const config = /*#__PURE__*/configuration({
-  theme,                                  // --> add the theme. modify `./theme.ts` for chaning the theme.
-  
+export const config = configuration({
+  theme,
+  src: {                           // @see /docs/config/entry
+    base: 'docs/md',               // --> the base folder for all markdowns
+    not_found: '404.md',           // --> markdown file for 404 page, relative to `base`
+    toc: '_toc.md',                // --> markdown file for toc, relative to `base`
+    pick: /\.md$/,                 // --> which files to pick (default: .md files)
+    drop: /(^_)|(\/_)/,            // --> which files to drop (default: _something.md files)
+  },
+  dest: {                          // @see /docs/config/output
+    html: 'dist',                  // --> the base folder for HTML files
+    assets: 'dist',                // --> the base folder for assets
+    namespace: '/p5.quadrille.js', // --> project namespace
+  },
   page: {
     title: {
-      base: 'P5.quadrille.js'             // --> the base title of your doc pages
+      base: 'p5.quadrille.js'      // --> the base title of your doc pages
     }
   },
-  
+  misc: {
+    github: {
+      user: 'objetos',             // --> name of the user on GitHub owning the repo
+      repo: 'p5.quadrille.js',     // --> name of the repo on GitHub
+      action: 'Star',              // --> action of the GitHub button
+      count: false,                // --> whether to show the `count` on the GitHub button
+      large: true,                 // --> whether to show a `large` GitHub button
+      standardIcon: false,         // --> whether to use the GitHub icon on the GitHub button or use an action specific icon
+    }
+  },  
 });
