dec77d5bf3649346afba257bf13bd5f9f14a59bb
nakednous <nakednous@gmail.com>
2020-11-21 17:53:24 -0500

    board example updated

--- examples/memory/sketch.js
+++ examples/memory/sketch.js
@@ -3,28 +3,62 @@ const COLS = 10;
 const LENGTH = 20;
 var quadrille;
 var board;
+var x = 2, y = 2;
 
 function setup() {
   createCanvas(COLS * LENGTH, ROWS * LENGTH);
   board = createBoard(ROWS, COLS);
-  quadrille = createQuadrille([[color('cyan'), '👽',             0    ],
-                               [0,             '🤔',            '🙈' ],
-                               [0,             color('#770811'), 0   ],
-                               ['g',           'o',             'l'  ]
-                              ]);
-  glue(quadrille, 2, 2);
+  quadrille = createQuadrille([[color('cyan'), '👽', 0],
+  [0, '🤔', '🙈'],
+  [0, color('#770811'), 0],
+  ['g', 'o', 'l']
+  ]);
 }
 
 function draw() {
   background('#060621');
-  //drawQuadrille(quadrille, 0, 0, LENGTH);
   drawBoard(board, LENGTH);
+  drawQuadrille(quadrille, x, y, LENGTH, 2, 'green');
 }
 
-function glue(quadrille, row, col) {
-  let update = board.add(quadrille, row, col);
-  if (update.memoryHitCounter === 0) {
-    //board.memory2D = update.quadrille.memory2D;
-    board = update.quadrille;
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    quadrille.reflect();
+  } else if (keyCode === RIGHT_ARROW) {
+    quadrille.rotate();
+  }
+  if (key === 'a') {
+    x--;
+  }
+  if (key === 's') {
+    x++;
+  }
+  if (key === 'w') {
+    y--;
+  }
+  if (key === 'z') {
+    y++;
+  }
+  if (key === 'g') {
+    glue(quadrille, y, x, false);
+  }
+  if (key === 'v') {
+    glue(quadrille, y, x);
+  }
+}
+
+function glue(quadrille, row, col, validate = true) {
+  if (validate) {
+    try {
+      let update = board.add(quadrille, row, col);
+      if (update.memoryHitCounter === 0) {
+        board = update.quadrille;
+      }
+    } catch (out_of_bounds) {
+      console.log(out_of_bounds);
+    }
+  }
+  else {
+    board = board.add(quadrille, row, col).quadrille;
   }
 }
