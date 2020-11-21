14134741d4426573f692cacb896395f95e1359f8
nakednous <nakednous@gmail.com>
2020-11-21 18:46:26 -0500

    board closure

--- docs/sketches/board.js
+++ docs/sketches/board.js
@@ -4,6 +4,7 @@ var myp5 = new p5((p) => {
   const LENGTH = 20;
   var quadrille;
   var board;
+  var x = 2, y = 2;
 
   p.setup = function () {
     p.createCanvas(COLS * LENGTH, ROWS * LENGTH);
@@ -13,28 +14,53 @@ var myp5 = new p5((p) => {
                                    [0,                p.color('#770811'), 0  ],
                                    ['g',              'o',                'l' ]
                                   ]);
-    this.glue(quadrille, 2, 2);    
   };
 
   p.draw = function () {
     p.background('#859900');
-    //p.drawQuadrille(quadrille, 0, 0, LENGTH);
-    p.drawBoard(board, LENGTH);    
+    p.drawBoard(board, LENGTH);
+    p.drawQuadrille(quadrille, x, y, LENGTH, 2, 'green');
   };
 
   p.keyPressed = function () {
-    if (p.keyCode === p.UP_ARROW) {
-      q.reflect();
-    } else if (p.keyCode === p.DOWN_ARROW) {
-      q.rotate();
+    if (p.keyCode === p.LEFT_ARROW) {
+      quadrille.reflect();
+    } else if (p.keyCode === p.RIGHT_ARROW) {
+      quadrille.rotate();
+    }
+    if (p.key === 'a') {
+      x--;
+    }
+    if (p.key === 's') {
+      x++;
+    }
+    if (p.key === 'w') {
+      y--;
+    }
+    if (p.key === 'z') {
+      y++;
+    }
+    if (p.key === 'g') {
+      glue(quadrille, y, x, false);
+    }
+    if (p.key === 'v') {
+      glue(quadrille, y, x);
     }
   };
 
-  p.glue = function (quadrille, row, col) {
-    let update = board.add(quadrille, row, col);
-    if (update.memoryHitCounter === 0) {
-      //board.memory2D = update.quadrille.memory2D;
-      board = update.quadrille;
-    }    
+  this.glue = function (quadrille, row, col, validate = true) {
+    if (validate) {
+      try {
+        let update = board.add(quadrille, row, col);
+        if (update.memoryHitCounter === 0) {
+          board = update.quadrille;
+        }
+      } catch (out_of_bounds) {
+        console.log(out_of_bounds);
+      }
+    }
+    else {
+      board = board.add(quadrille, row, col).quadrille;
+    }
   };
 }, "board");
