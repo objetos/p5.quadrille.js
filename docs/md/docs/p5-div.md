df24ccdbc650d76b3066a0ad076528cff5e6c3e2
nakednous <nakednous@gmail.com>
2020-12-16 15:41:13 -0500

    examples updated

--- docs/md/docs/p5-div.md
+++ docs/md/docs/p5-div.md
@@ -1,3 +1,3 @@
 # P5 div use
 
-> :P5 sketch=/docs/sketches/colors.js, width=200, height=400, version=1.1.8
+> :P5 sketch=/docs/sketches/colors.js, width=400, height=400, version=1.1.8
--- docs/sketches/colors.js
+++ docs/sketches/colors.js
@@ -1,48 +1,45 @@
-const ROWS = 20;
-const COLS = 10;
-const LENGTH = 20;
-var quadrille;
-var clone;
-var x = 2, y = 2;
+var myp5 = new p5((p) => {
+  const ROWS = 20;
+  const COLS = 20;
+  const LENGTH = 20;
+  var quadrille;
+  var clone;
+  var x = 2, y = 2;
 
-function setup() {
-  createCanvas(COLS * LENGTH, ROWS * LENGTH);
-  quadrille = createQuadrille([[color('cyan'), '👽',             0    ],
-                               [0,             '🤔',            '🙈' ],
-                               [0,             color('#770811'), 0   ],
-                               ['g',           'o',             'l'  ]
-                              ]);
-  quadrille.reflect();
-  clone = quadrille.clone();
-  clone.reflect();
-}
+  p.setup = function () {
+    p.createCanvas(COLS * LENGTH, ROWS * LENGTH);
+    quadrille = p.createQuadrille([[p.color('cyan'), '👽',               0    ],
+                                   [0,               '🤔',              '🙈' ],
+                                   [0,               p.color('#770811'), 0   ],
+                                   ['g',             'o',                'l' ]
+                                  ]);
+    clone = quadrille.clone();
+    clone.reflect();
+  };
 
-function draw() {
-  background('#060621');
-  drawQuadrille(quadrille, x, y, LENGTH, 2, 'green');
-  drawQuadrille(clone, 2, 8, LENGTH, 0);
-}
+  p.draw = function () {
+    p.background('#007ACC');
+    p.drawQuadrille(quadrille, x, y, LENGTH, 2, 'green');
+    p.drawQuadrille(clone, 12, 2, LENGTH, 0);
+  };
 
-function keyPressed() {
-  if (keyCode === UP_ARROW) {
-    quadrille.reflect();
-  } else if (keyCode === DOWN_ARROW) {
-    quadrille.rotate();
-  }
-  if (key === 'a') {
-    x = x > 0 ? x - 1 : x;
-  }
-  if (key === 's') {
-    x = x < COLS - quadrille.width ? x + 1 : x;
-  }
-  if (key === 'w') {
-    y = y > 0 ? y - 1 : y;
-  }
-  if (key === 'z') {
-    y = y < ROWS - quadrille.height ? y + 1 : y;
-  }
-}
-
-function debugQuadrille(quadrille) {
-  console.log(quadrille.shape);
-}
+  p.keyPressed = function () {
+    if (p.keyCode === p.LEFT_ARROW) {
+      quadrille.reflect();
+    } else if (p.keyCode === p.RIGHT_ARROW) {
+      quadrille.rotate();
+    }
+    if (p.key === 'a') {
+      x = x > 0 ? x - 1 : x;
+    }
+    if (p.key === 's') {
+      x = x < COLS - quadrille.width ? x + 1 : x;
+    }
+    if (p.key === 'w') {
+      y = y > 0 ? y - 1 : y;
+    }
+    if (p.key === 'z') {
+      y = y < ROWS - quadrille.height ? y + 1 : y;
+    }
+  };
+}, "colors");
