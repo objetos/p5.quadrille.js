e030bde7f7bc5734db9cd18c801be3c0e7be57db
nakednous <nakednous@gmail.com>
2021-01-30 16:56:50 -0500

    better component size

--- docs/md/docs/demos/image.md
+++ docs/md/docs/demos/image.md
@@ -1,7 +1,5 @@
 # Images
 
-image = loadImage('/p5.quadrille.js/docs/sketches/mahakala.jpg')
-
 > :P5 lib1=https://cdn.jsdelivr.net/gh/objetos/p5.quadrille.js/p5.quadrille.min.js, sketch=/docs/sketches/image.js, width=800, height=342
 
 > :Collapse label=the board.js running in p5 global mode
--- docs/sketches/image.js
+++ docs/sketches/image.js
@@ -1,23 +1,20 @@
-//var scl = 20;
 var scl = 0;
 var quadrille;
 var image;
 
 function preload() {
   image = loadImage('/p5.quadrille.js/docs/sketches/mahakala.jpg');
-  //image = loadImage('abraham_lincoln.jpg');
 }
 
 function setup() {
   createCanvas(800, 342);
-  //console.log(image.width, image.height);
   quadrille = createQuadrille(40 * (2 ** scl), image);
-  //console.log(Quadrille.version);
 }
 
 function draw() {
   background('#060621');
   drawQuadrille(quadrille, 0, 0, 20 / (2 ** scl), 1.6 / (2 ** scl));
+  //if (frameCount)
 }
 
 function keyPressed() {
