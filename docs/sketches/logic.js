be87b3ff1e91f9677a097db2c206c9886a94b7f6
nakednous <nakednous@gmail.com>
2021-01-30 18:58:44 -0500

    better logic example

--- docs/sketches/logic.js
+++ docs/sketches/logic.js
@@ -2,10 +2,10 @@ const ROWS = 10;
 const COLS = 20;
 const LENGTH = 40;
 var q1, q2, q3;
-var a2, a3;
+var a1 = true;
 var x1 = 4, y1 = 2;
 var x2 = 14, y2 = 5;
-var x3 = 0, y3 = 0;
+var x3, y3;
 var c1, c2, c3;
 
 function setup() {
@@ -27,10 +27,12 @@ function setup() {
 
 function draw() {
   background('#060621');
-  drawQuadrille(q1, x1, y1, LENGTH, 2, 'green', true);
-  drawQuadrille(q2, x2, y2, LENGTH, 2, 'blue', true);
   if (q3) {
     drawQuadrille(q3, x3, y3, LENGTH, 2, 'magenta', true);
+  }
+  else {
+    drawQuadrille(q1, x1, y1, LENGTH, 2, 'green', true);
+    drawQuadrille(q2, x2, y2, LENGTH, 2, 'blue', true);
   }
 }
 
@@ -42,50 +44,47 @@ function keyPressed() {
   }
   if (key == 'c') {
     q3 = undefined;
-    x3 = y3 = 0;
-    if (a3) {
-      a3 = false;
-    }
   }
   if (key === '1') {
-    a2 = false;
-    a3 = false;
+    a1 = true;
   }
   if (key === '2') {
-    a2 = true;
-    a3 = false;
-  }
-  if (key === '3') {
-    a2 = false;
-    a3 = true;
+    a1 = false;
   }
   if (key === 'a') {
-    a2 ? x2-- : a3 ? x3-- : x1--;
+    q3 ? x3-- : a1 ? x1-- : x2--;
   }
   if (key === 's') {
-    a2 ? x2++ : a3 ? x3++ : x1++;
+    q3 ? x3++ : a1 ? x1++ : x2++;
   }
   if (key === 'w') {
-    a2 ? y2-- : a3 ? y3-- : y1--;
+    q3 ? y3-- : a1 ? y1-- : y2--;
   }
   if (key === 'z') {
-    a2 ? y2++ : a3 ? y3++ : y1++;
+    q3 ? y3++ : a1 ? y1++ : y2++;
   }
   if (key === 'o') {
     q3 = Quadrille.OR(q1, q2, y2-y1, x2-x1);
+    x3 = x1 < x2 ? x1 : x1 + x2-x1;
+    y3 = y1 < y2 ? y1 : y1 + y2-y1;
   }
   if (key === 'n') {
     q3 = Quadrille.AND(q1, q2, y2-y1, x2-x1);
+    x3 = x1 < x2 ? x1 : x1 + x2-x1;
+    y3 = y1 < y2 ? y1 : y1 + y2-y1;
   }
   if (key === 'x') {
     q3 = Quadrille.XOR(q1, q2, y2-y1, x2-x1);
+    x3 = x1 < x2 ? x1 : x1 + x2-x1;
+    y3 = y1 < y2 ? y1 : y1 + y2-y1;
   }
   if (key === 'd') {
     q3 = Quadrille.DIFF(q1, q2, y2-y1, x2-x1);
+    x3 = x1 < x2 ? x1 : x1 + x2-x1;
+    y3 = y1 < y2 ? y1 : y1 + y2-y1;
   }
   if (key == 'm') {
     q1.clear(1);
-    console.log(q1.height);
   }
   if (key == 'i') {
     q1.insert(1);
