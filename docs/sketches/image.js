f28ce2f5918e8f4eeba5479f750367b70a6571f9
nakednous <nakednous@gmail.com>
2021-11-24 13:23:08 -0500

    intro sketch updated

--- docs/sketches/image.js
+++ docs/sketches/image.js
@@ -13,6 +13,6 @@ function draw() {
   if (frameCount % 200 === 0) {
     let scl = 2 ** int(random(4));
     quadrille = createQuadrille(20 * scl, image);
-    drawQuadrille(quadrille, 0, 0, 40 / scl, 1.6 / scl, color(random(255)));
+    drawQuadrille(quadrille, {cellLength: 40 / scl, outlineWeight: 1.6 / scl, outline: color(random(255))});
   }
 }
