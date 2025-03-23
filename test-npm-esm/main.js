cbddbff40d1aca01da9391068593250a2fe2e975
nakednous <nakednous@gmail.com>
2025-03-22 22:46:23 -0500

    vite test update (using patch)

--- test-npm-esm/main.js
+++ test-npm-esm/main.js
@@ -1,12 +1,18 @@
 import p5 from 'p5'
+import Quadrille from 'p5.quadrille'
+
+let q
 
 const sketch = (p) => {
   p.setup = () => {
     p.createCanvas(400, 400)
+    q = p.createQuadrille(4, 4)
+    q.fill(1, 1, p.color('red'))
   }
 
   p.draw = () => {
-    p.background('skyblue')
+    p.background('blue')
+    p.drawQuadrille(q, {outline: 'grey'})
   }
 }
 
--- test-npm-esm/vite.config.js
+++ test-npm-esm/vite.config.js
@@ -1,11 +1,11 @@
-// test-npm-esm/vite.config.js
 import { defineConfig } from 'vite'
 import path from 'path'
 
 export default defineConfig({
   resolve: {
     alias: {
-      'p5': path.resolve(__dirname, '../node_modules/p5/lib/p5.esm.js')
+      'p5': path.resolve(__dirname, '../node_modules/p5/lib/p5.esm.js'),
+      'p5.quadrille': path.resolve(__dirname, '../dist/p5.quadrille.esm.js')
     }
   }
 })
